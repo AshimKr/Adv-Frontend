d81ff03541724d73064373b60b86c1bd5b1d967d
AshimKr <anonymousone679@gmail.com>
2022-07-18 22:28:12 +0530

    id Card

--- ID Card Generator/script.js
+++ ID Card Generator/script.js
@@ -1,19 +1,39 @@
 const fileChoose = document.getElementById('chooseFile')
 const preview = document.getElementById('preview')
 
-console.log(fileChoose);
 fileChoose.addEventListener('change',()=>{
-    console.log('skkjskm');
+    const files = fileChoose.files[0];
+    if (files) {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(files);
+        fileReader.addEventListener("load", function () {
+            preview.style.display = "block";
+            preview.innerHTML = '<img class="img-preview" src="' + this.result + '" />';
+        });    
+  }
 })
 
-// fileChoose.addEventListener('change',()=>{
-//     const files = fileChoose.files[0];
-//     if (files) {
-//         const fileReader = new FileReader();
-//         fileReader.readAsDataURL(files);
-//         fileReader.addEventListener("load", function () {
-//             preview.style.display = "block";
-//             preview.innerHTML = '<img src="' + this.result + '" />';
-//         });    
-//   }
-// })
+
+// card generation code
+const button = document.getElementsByClassName('btn')[0];
+
+let studentName = document.getElementsByTagName('input')[0];
+let studentCollege= document.getElementsByTagName('input')[1];
+let studentLocation = document.getElementsByTagName('input')[2];
+
+const cardName = document.getElementById('name');
+const cardCollege = document.getElementById('clg');
+const cardLocation = document.getElementById('location');
+const cardImage = document.getElementById('cardImage');
+    
+button.addEventListener('click',()=>{
+    cardName.innerHTML = studentName.value;
+    cardCollege.innerHTML = studentCollege.value;
+    cardLocation.innerHTML = studentLocation.value;
+
+    cardImage.src=document.getElementsByClassName('img-preview')[0].src
+    
+    document.getElementById('MainCard').style.display='block';
+    document.getElementById('msg').style.display='none';
+
+})
